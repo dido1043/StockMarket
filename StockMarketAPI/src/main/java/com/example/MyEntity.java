205c84fd7a8fb2272376bfc61b7303392f00a160
dido1043 <dudunovski_05@abv.bg>
2025-08-27 15:14:57 +0300

    Initialized applicatopn

---
+++ StockMarketAPI/src/main/java/com/example/MyEntity.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+
+
+@Entity
+public class MyEntity extends PanacheEntity {
+    public String field;
+}
+
