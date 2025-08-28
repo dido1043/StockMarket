ccffc87cd5b7e0044de1b662a348b4462dfd5082
dido1043 <dudunovski_05@abv.bg>
2025-08-28 17:19:07 +0300

    Created stock entity and dto

--- StockMarketAPI/src/main/java/com/example/model/dto/StockDto.java
+++ StockMarketAPI/src/main/java/com/example/model/dto/StockDto.java
@@ -1,4 +1,37 @@
 package com.example.model.dto;
 
+import com.example.model.entity.Company;
+import jakarta.persistence.Column;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class StockDto {
+    private Company company;
+    private BigDecimal marketCapitalization;
+    private BigDecimal shareOutstanding;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public BigDecimal getMarketCapitalization() {
+        return marketCapitalization;
+    }
+
+    public void setMarketCapitalization(BigDecimal marketCapitalization) {
+        this.marketCapitalization = marketCapitalization;
+    }
+
+    public BigDecimal getShareOutstanding() {
+        return shareOutstanding;
+    }
+
+    public void setShareOutstanding(BigDecimal shareOutstanding) {
+        this.shareOutstanding = shareOutstanding;
+    }
 }
--- StockMarketAPI/src/main/java/com/example/model/entity/Stock.java
+++ StockMarketAPI/src/main/java/com/example/model/entity/Stock.java
@@ -3,14 +3,52 @@ package com.example.model.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "stock")
 public class Stock  extends PanacheEntity {
 
     @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "companyId", nullable = false)
-    public Company company;
+    @JoinColumn(name = "company", nullable = false)
+    private Company company;
+    @Column(name = "market_capitalization", precision = 20, scale = 2, nullable = false)
+    private BigDecimal marketCapitalization;
+    @Column(name = "share_outstanding", precision = 20, scale = 2, nullable = false)
+    private BigDecimal shareOutstanding;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 
+    public Company getCompany() {
+        return company;
+    }
 
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 
+    public BigDecimal getMarketCapitalization() {
+        return marketCapitalization;
+    }
+
+    public void setMarketCapitalization(BigDecimal marketCapitalization) {
+        this.marketCapitalization = marketCapitalization;
+    }
+
+    public BigDecimal getShareOutstanding() {
+        return shareOutstanding;
+    }
+
+    public void setShareOutstanding(BigDecimal shareOutstanding) {
+        this.shareOutstanding = shareOutstanding;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
